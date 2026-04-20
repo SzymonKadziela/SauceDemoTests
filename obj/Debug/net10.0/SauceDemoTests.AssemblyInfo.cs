d58518921a9f65cab56cded83e357d2ff0e38dcf
Szymon Kądziela <szwarc840@gmail.com>
2026-04-20 20:08:26 +0200

    cleaning

--- obj/Debug/net10.0/SauceDemoTests.AssemblyInfo.cs
+++ obj/Debug/net10.0/SauceDemoTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SauceDemoTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+904396d01d97b3f1103f236356db9bb7d637d44a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7174a93366653cf24d197740178b778956af3f61")]
 [assembly: System.Reflection.AssemblyProductAttribute("SauceDemoTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SauceDemoTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
